8daec930d4b768e0ecd8f6a18ab2b055191c159f
Matias <matiasmonti2011@hotmail.com>
2023-07-24 18:07:45 -0300

    Add AudioManager

---
+++ Examen_Final_Monti_Matias/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource[] sfxSources;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayMusic(AudioClip musicClip)
+    {
+        musicSource.clip = musicClip;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip sfxClip)
+    {
+        foreach (var sfxSource in sfxSources)
+        {
+            if (!sfxSource.isPlaying)
+            {
+                sfxSource.clip = sfxClip;
+                sfxSource.Play();
+                return;
+            }
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        foreach (var sfxSource in sfxSources)
+        {
+            sfxSource.volume = volume;
+        }
+    }
+
+    public float GetMusicVolume()
+    {
+        return musicSource.volume;
+    }
+
+    public float GetSFXVolume()
+    {
+        return sfxSources[0].volume;
+    }
+
+    public void MuteAllAudio(bool isMuted)
+    {
+        AudioListener.pause = isMuted;
+    }
+}
