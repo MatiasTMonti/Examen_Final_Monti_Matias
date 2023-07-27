b5a68df99d2e0dd754f9fbbd0f89ef21b52fef85
Matias <matiasmonti2011@hotmail.com>
2023-07-27 19:29:21 -0300

    Change Script Camera

--- Examen_Final_Monti_Matias/Assets/Scripts/Player/Camera/TankCamera.cs
+++ Examen_Final_Monti_Matias/Assets/Scripts/Player/Camera/TankCamera.cs
@@ -5,18 +5,36 @@ namespace tankDefend
     public class TankCamera : MonoBehaviour
     {
         [SerializeField] private Transform target;
-        [SerializeField] private Vector3 offset = new Vector3(0f, 6f, -10f);
+        [SerializeField] private Vector3 offset;
         [SerializeField] private float smoothSpeed = 0.125f;
+        [SerializeField] private float mouseSensitivity = 2f;
+
+        private Vector3 desiredPosition;
+        private Vector3 smoothedPosition;
 
         private void LateUpdate()
         {
-            Vector3 desiredPosition = target.position + offset;
+            if (target != null)
+            {
+                HandleMouseInput();
+                desiredPosition = target.position + offset;
+                smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+                transform.position = smoothedPosition;
 
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+                transform.LookAt(target);
+            }
+        }
 
-            transform.position = smoothedPosition;
+        private void HandleMouseInput()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+                float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-            transform.LookAt(target);
+                Vector3 newOffset = Quaternion.Euler(0f, mouseX, 0f) * offset;
+                offset = Quaternion.Euler(-mouseY, 0f, 0f) * newOffset;
+            }
         }
     }
 }
